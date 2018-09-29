24c3cd2ada97aaf37411aec333b401fa81954f4d
Antonio Pagano <apagano@wawand.co>
2018-09-29 12:37:58 -0500

    updated to use transformer

--- genny/auth/auth.go
+++ genny/auth/auth.go
@@ -3,13 +3,13 @@ package auth
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/gobuffalo/buffalo/meta"
-	"github.com/gobuffalo/flect"
 	"github.com/gobuffalo/genny"
+	"github.com/gobuffalo/genny/movinglater/attrs"
 	"github.com/gobuffalo/genny/movinglater/gotools"
 	"github.com/gobuffalo/genny/movinglater/plushgen"
 	"github.com/gobuffalo/packr"
@@ -17,183 +17,110 @@ import (
 	"github.com/pkg/errors"
 )
 
+func extraAttrs(args []string) []string {
+	var names = map[string]string{
+		"email":    "email",
+		"password": "password",
+		"id":       "id",
+	}
+
+	var result = []string{}
+	for _, field := range args {
+		at, _ := attrs.Parse(field)
+		field = at.Name.Underscore().String()
+
+		if names[field] != "" {
+			continue
+		}
+
+		names[field] = field
+		result = append(result, field)
+	}
+
+	return result
+}
+
+var fields attrs.Attrs
+
 // New actions/auth.go file configured to the specified providers.
 func New(args []string) (*genny.Generator, error) {
 	g := genny.New()
 
-	fields, extraFields := extractFields(args)
+	var err error
+	fields, err = attrs.ParseArgs(extraAttrs(args)...)
+	if err != nil {
+		return g, errors.WithStack(err)
+	}
 
-	parts := append([]string{"db", "generate", "model"}, fields...)
-	g.Command(exec.Command("buffalo", parts...))
-
-	if err := g.Box(packr.NewBox("./templates")); err != nil {
+	if err := g.Box(packr.NewBox(filepath.Join("..", "auth", "templates"))); err != nil {
 		return g, errors.WithStack(err)
 	}
 
 	ctx := plush.NewContext()
 	ctx.Set("app", meta.New("."))
+	ctx.Set("attrs", fields)
 
 	g.Transformer(plushgen.Transformer(ctx))
-	g.RunFn(modifyUsersModel)
-	g.RunFn(addPasswordFields)
-	g.RunFn(addAppActions)
-	g.RunFn(formFieldsFn(extraFields))
+	g.Transformer(genny.NewTransformer(".html", newUserHTMLTransformer))
+	g.Transformer(genny.NewTransformer(".fizz", migrationsTransformer))
 
-	return g, nil
-}
-
-func extractFields(args []string) ([]string, []string) {
-	fields := []string{"user", "email", "password_hash"}
-	extraFields := []string{}
-	for _, field := range args {
-		fieldName := strings.Split(field, ":")[0]
-		if strings.Contains(strings.Join(fields, "\n"), fieldName) {
-			continue
-		}
-
-		fields = append(fields, field)
-		extraFields = append(extraFields, field)
-	}
-
-	return fields, extraFields
-}
-
-func formFieldsFn(extraFields []string) genny.RunFn {
-	return func(r *genny.Runner) error {
-		fieldInputs := []string{}
-		for _, field := range extraFields {
-			name := flect.Capitalize(flect.Camelize(field))
-			fieldInputs = append(fieldInputs, fmt.Sprintf(`<%%= f.InputTag("%v", {}) %%>`, name))
-		}
-
-		path := filepath.Join("templates", "users", "new.html")
-		f, err := os.Open(path)
+	g.RunFn(func(r *genny.Runner) error {
+		path := filepath.Join("actions", "app.go")
+		file, err := os.Open(path)
 		if err != nil {
 			return err
 		}
 
-		gf := genny.NewFile(path, f)
-		lines := strings.Split(gf.String(), "\n")
-		ln := -1
+		gf := genny.NewFile(path, file)
 
-		for index, line := range lines {
-			if strings.Contains(line, `<%= f.InputTag("PasswordConfirmation", {type: "password"}) %>`) {
-				ln = index
-				break
-			}
-		}
+		gf, err = gotools.AddInsideBlock(
+			gf,
+			`if app == nil {`,
+			`app.Use(SetCurrentUser)`,
+			`app.Use(Authorize)`,
+			`app.GET("/users/new", UsersNew)`,
+			`app.POST("/users", UsersCreate)`,
+			`app.GET("/signin", AuthNew)`,
+			`app.POST("/signin", AuthCreate)`,
+			`app.DELETE("/signout", AuthDestroy)`,
+			`app.Middleware.Skip(Authorize, HomeHandler, UsersNew, UsersCreate, AuthNew, AuthCreate)`,
+		)
 
-		lines = append(lines[:ln], append(fieldInputs, lines[ln:]...)...)
-		gf = genny.NewFile(path, strings.NewReader(strings.Join(lines, "\n")))
 		return r.File(gf)
-	}
+	})
+
+	return g, nil
 }
 
-func addAppActions(r *genny.Runner) error {
-	f, err := os.Open(`actions/app.go`)
-	if err != nil {
-		return err
+func newUserHTMLTransformer(f genny.File) (genny.File, error) {
+	if f.Name() != filepath.Join("templates", "users", "new.html") {
+		return f, nil
 	}
 
-	file := genny.NewFile(`actions/app.go`, f)
-	file, err = gotools.AddInsideBlock(
-		file,
-		"if app == nil {",
-		`app.Use(SetCurrentUser)`,
-		`app.Use(Authorize)`,
-		`app.GET("/users/new", UsersNew)`,
-		`app.POST("/users", UsersCreate)`,
-		`app.GET("/signin", AuthNew)`,
-		`app.POST("/signin", AuthCreate)`,
-		`app.DELETE("/signout", AuthDestroy)`,
-		`app.Middleware.Skip(Authorize, HomeHandler, UsersNew, UsersCreate, AuthNew, AuthCreate)`,
-	)
-
-	if err != nil {
-		return err
+	fieldInputs := []string{}
+	for _, field := range fields {
+		name := field.Name.Proper()
+		fieldInputs = append(fieldInputs, fmt.Sprintf(`<%%= f.InputTag("%v", {}) %%>`, name))
 	}
 
-	return r.File(file)
-}
+	lines := strings.Split(f.String(), "\n")
+	ln := -1
 
-func addPasswordFields(r *genny.Runner) error {
-	f, err := os.Open(`models/user.go`)
-	if err != nil {
-		return err
-	}
-	file := genny.NewFile(`models/user.go`, f)
-	file, err = gotools.AddInsideBlock(
-		file,
-		"type User struct {",
-		"Password string `json:\"-\" db:\"-\"`",
-		"PasswordConfirmation string `json:\"-\" db:\"-\"`",
-	)
-
-	if err != nil {
-		return err
-	}
-
-	return r.File(file)
-}
-
-func modifyUsersModel(r *genny.Runner) error {
-	f, err := os.Open(`models/user.go`)
-	if err != nil {
-		return err
-	}
-
-	file := genny.NewFile(`models/user.go`, f)
-	file = gotools.ReplaceBlockBody(file, `func (u *User) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {`, validateCreateFuncBodyLiteral)
-	file = gotools.ReplaceBlockBody(file, "func (u *User) Validate(tx *pop.Connection) (*validate.Errors, error) {", validateFuncBodyLiteral)
-	file = gotools.Append(createFuncLiteral)
-	file, err = gotools.AddImport(file, "strings", "github.com/pkg/errors", "golang.org/x/crypto/bcrypt")
-
-	return r.File(file)
-}
-
-const (
-	createFuncLiteral = `
-	// Create wraps up the pattern of encrypting the password and
-	// running validations. Useful when writing tests.
-	func (u *User) Create(tx *pop.Connection) (*validate.Errors, error) {
-		u.Email = strings.ToLower(strings.TrimSpace(u.Email))
-		ph, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
-		if err != nil {
-			return validate.NewErrors(), errors.WithStack(err)
+	for index, line := range lines {
+		if strings.Contains(line, `<%= f.InputTag("PasswordConfirmation"`) {
+			ln = index + 1
+			break
 		}
-		u.PasswordHash = string(ph)
-		return tx.ValidateAndCreate(u)
-	}`
+	}
 
-	validateFuncBodyLiteral = `
-		var err error
-		return validate.Validate(
-			&validators.StringIsPresent{Field: u.Email, Name: "Email"},
-			&validators.StringIsPresent{Field: u.PasswordHash, Name: "PasswordHash"},
-			// check to see if the email address is already taken:
-			&validators.FuncValidator{
-				Field:   u.Email,
-				Name:    "Email",
-				Message: "%s is already taken",
-				Fn: func() bool {
-					var b bool
-					q := tx.Where("email = ?", u.Email)
-					if u.ID != uuid.Nil {
-						q = q.Where("id != ?", u.ID)
-					}
-					b, err = q.Exists(u)
-					if err != nil {
-						return false
-					}
-					return !b
-				},
-			},
-		), err`
+	lines = append(lines[:ln], append(fieldInputs, lines[ln:]...)...)
+	b := strings.NewReader(strings.Join(lines, "\n"))
+	return genny.NewFile(f.Name(), b), nil
+}
 
-	validateCreateFuncBodyLiteral = `
-		var err error
-		return validate.Validate(
-			&validators.StringIsPresent{Field: u.Password, Name: "Password"},
-			&validators.StringsMatch{Name: "Password", Field: u.Password, Field2: u.PasswordConfirmation, Message: "Password does not match confirmation"},
-		), err`
-)
+func migrationsTransformer(f genny.File) (genny.File, error) {
+	t := time.Now()
+	parts := strings.Split(f.Name(), string(filepath.Separator))
+	p := parts[len(parts)-1]
+	return genny.NewFile(filepath.Join("migrations", fmt.Sprintf("%d_%s", t.UnixNano(), p)), f), nil
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/gobuffalo/buffalo-auth
 require (
 	github.com/gobuffalo/buffalo v0.12.6
 	github.com/gobuffalo/buffalo-plugins v0.0.0-20180924041210-6ea00eeaef92
-	github.com/gobuffalo/flect v0.0.0-20180907193754-dc14d8acaf9f
-	github.com/gobuffalo/genny v0.0.0-20180926172232-22498a7facf1
+	github.com/gobuffalo/genny v0.0.0-20180928214617-9033ae798a5a
 	github.com/gobuffalo/packr v1.13.7
 	github.com/gobuffalo/plush v3.7.16+incompatible
 	github.com/pkg/errors v0.8.0
 	github.com/sirupsen/logrus v1.1.0
 	github.com/spf13/cobra v0.0.3
 	github.com/stretchr/testify v1.2.2
+	golang.org/x/crypto v0.0.0-20180927165925-5295e8364332 // indirect
+	golang.org/x/sys v0.0.0-20180928133829-e4b3c5e90611 // indirect
+	golang.org/x/tools v0.0.0-20180928181343-b3c0be4c978b // indirect
 )
